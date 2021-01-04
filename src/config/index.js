95213284e9cae75f1a374ec924e9470054f14514
Romain Guilloteau <rubyistdotjs@gmail.com>
2021-01-04 09:57:36 +0100

    Update js-yaml to 4.0

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,7 @@ const environement = process.env.STAGE || 'development';
 
 const configFilepath = path.resolve(__dirname, `./${environement}.yml`);
 const configFile = fs.readFileSync(configFilepath, 'utf8');
-const config = yaml.safeLoad(configFile);
+const config = yaml.load(configFile);
 
 const developers = config.teams.map((team) => team.members).flat();
 const developerGithubUsernames = developers.map((dev) => dev.githubUsername);
